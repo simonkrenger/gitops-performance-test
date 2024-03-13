a24cae61a8f062ff0889c7eb9e93e5ed80ff69cf
Simon Krenger <skrenger@redhat.com>
2024-03-13 11:07:49 +0100

    Recreate instances, increase to 250 instances

--- generate_instances.sh
+++ generate_instances.sh
@@ -4,7 +4,7 @@ wd=$(dirname $(readlink -f $0))
 
 rm -rf ${wd}/instances/*
 
-for i in $(seq 1 120); do
+for i in $(seq 1 250); do
     id=$(echo $RANDOM | md5sum | head -c 10; echo)
     mkdir -p ${wd}/instances/$id
     echo "{ 'id': '${id}' }" > ${wd}/instances/$id/config.json
